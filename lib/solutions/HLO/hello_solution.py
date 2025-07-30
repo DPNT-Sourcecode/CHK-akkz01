64fba64fa20e2e77a4e2e91b7ea58c857fb3d8aa
sbx_user1051 <sbx_user1051@169.254.15.93>
2025-07-30 10:51:31 +0000

    Wed Jul 30 10:51:31 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,11 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name):
-        raise NotImplementedError()
+        try:
+            if friend_name is None or not isinstance(friend_name, str):
+                raise NotImplementedError()
+            else:
+                return f"Hello, {friend_name}"
+        except Exception as e:
+            return f"Error: {str(e)}"
+
