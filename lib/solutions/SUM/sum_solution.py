92d870c00946c872c4eb0531467372b9f31d159a
sbx_user1051 <sbx_user1051@169.254.15.93>
2025-07-30 10:47:32 +0000

    Wed Jul 30 10:47:32 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,8 +2,9 @@
 class SumSolution:
     
     def compute(self, x, y):
-        if x.isnumeric() and y.isnumeric():
-            return int(x) + int(y)
+        if isinstance(x, int) and isinstance(y, int):
+            return x + y
         else:
             raise ValueError("Both inputs must be intergers")
+
 
