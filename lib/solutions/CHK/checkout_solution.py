d49cb8289f2f1b9dc8d8f90199881c4a6225ec1c
sbx_user1051 <sbx_user1051@169.254.12.125>
2025-08-03 09:51:34 +0000

    Sun Aug 03 09:51:34 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ class CheckoutSolution:
         if not isinstance(skus, str):
             return -1  # Illegal Input
 
-        valid_skus = set('ABCDE')
+        valid_skus = set('ABCDEF')
         if any(c not in valid_skus for c in skus):
             return -1
 
@@ -15,7 +15,8 @@ class CheckoutSolution:
             'B': 30,
             'C': 20,
             'D': 15,
-            'E': 40
+            'E': 40,
+            'F': 10
         }
 
         from collections import Counter
@@ -33,6 +34,10 @@ class CheckoutSolution:
         free_b = sku_count['E'] // 2
         effective_b = max(0, sku_count['B'] - free_b)
 
+        # Apply F free F offer: For every 2 F, get one F free
+        free_f = sku_count['F'] // 2
+        effective_b = max(0, sku_count['F'] - free_f)
+
         # Apply B discount: 2 for 45
         total += (effective_b // 2) * 45
         total += (effective_b % 2) * prices['B']
@@ -43,6 +48,7 @@ class CheckoutSolution:
         total += sku_count['E'] * prices['E']
 
         return total
+
 
 
 
