c7cc104ea9672afdfacb1d691932d1b537859dfc
sbx_user1051 <sbx_user1051@169.254.12.125>
2025-08-03 09:29:41 +0000

    Sun Aug 03 09:29:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,19 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        if skus == "A":
+            return 50 
+        elif skus == "AAA":
+            return 130
+        elif skus == "B":
+            return 30
+        elif skus == "BB":
+            return 45  
+        elif skus == "C":
+            return 20
+        elif skus == "D":
+            return 15
+        else:
+            return -1 # Invalid SKU
+            raise NotImplementedError()
+
